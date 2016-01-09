67b0d5525c4ab151a11be55fc35d390707dc3289
sobota <michal.soboszek@gmail.com>
2016-01-09 14:18:15 +0100

    Small changes && fixes

--- src/main/scala/nkr/uj/lidar/graphics/GuiMain.scala
+++ src/main/scala/nkr/uj/lidar/graphics/GuiMain.scala
@@ -18,10 +18,10 @@ import scalafx.scene.paint.Color
   */
 object GuiMain extends JFXApp {
 
-  val (width, height) = (600.0, 600.0)
-  val (centerWidth, centerHeight) = (width / 2, height / 2)
+  val (windowWidth, windowHeight) = (600.0, 600.0)
+  val (centerWindowWidth, centerWindowHeight) = (windowWidth / 2, windowHeight / 2)
 
-  val canvas = new Canvas(width, height)
+  val canvas = new Canvas(windowWidth, windowHeight)
   implicit val gc = canvas.graphicsContext2D
 
   val dataProcessor = LidarDataProcessor(drawDistanceVector)
@@ -30,22 +30,6 @@ object GuiMain extends JFXApp {
   canvas.translateY = 0
 
   drawRobot()
-
-  /*
-      gc.beginPath()
-      gc.strokeLine(centerHeight, centerHeight, 30, 30)
-      gc.strokeLine(centerHeight, centerHeight, 30, -30)
-
-      gc.closePath()
-
-
-    drawDistanceVector((-4 * 100 * 10, -4 * 100 * 10))
-    drawDistanceVector((3 * 100 * 10, 3 * 100 * 10))
-
-    drawDistanceVector((-6 * 100 * 10, 6 * 100 * 10))
-    drawDistanceVector((6 * 100 * 10, -6 * 100 * 10))
-  drawDistanceVector((-0 * 100 * 10, 0 * 100 * 10))
-  */
 
   stage = new PrimaryStage {
     title = "Distance Vectors"
@@ -57,11 +41,13 @@ object GuiMain extends JFXApp {
         Platform.exit()
       }
     }
-    scene = new Scene(GuiMain.width, GuiMain.height) {
+    scene = new Scene(GuiMain.windowWidth, GuiMain.windowHeight) {
       content = canvas
     }
 
   }
+  val (canvasWidth, canvasHeight) = (canvas.getWidth, canvas.getHeight)
+  val (centerCanvasWidth, centerCanvasHeight) = (canvas.getWidth / 2, canvas.getHeight / 2) //todo repair canvas size caluculation
 
   def drawRobot()(implicit g: GraphicsContext) = {
 
@@ -69,20 +55,20 @@ object GuiMain extends JFXApp {
 
     g.beginPath()
     g.fill = Color.Green
-    g.fillRect((centerWidth - (roboWidth / 2)), (centerHeight - (roboHeigh / 2)), roboHeigh, roboHeigh)
+    g.fillRect((centerWindowWidth - (roboWidth / 2)), (centerWindowHeight - (roboHeigh / 2)), roboWidth, roboHeigh)
     g.closePath()
   }
 
   def drawDistanceVector(vectorEnd: ((Double, Double)) => (Double, Double)): Unit = {
 
-    val vectorEndPoint = vectorEnd(canvas.getWidth, canvas.getHeight)
+    val vectorEndPoint = vectorEnd(windowWidth, windowHeight)
 
     runLater {
 
       gc.beginPath()
       gc.lineWidth = 2
 
-      gc.strokeLine(centerWidth, centerHeight, vectorEndPoint._1, vectorEndPoint._2)
+      gc.strokeLine(centerWindowWidth, centerWindowHeight, vectorEndPoint._1, vectorEndPoint._2)
       gc.closePath()
     }
   }
